8665874dd9eeccd0f5da5165ffa3bfa96254ea0d
Enmanuel Delgado <102194318+enmanuel52@users.noreply.github.com>
2024-02-01 00:23:17 +0100

    Delete app/build directory
    A lot of generated files

--- app/build/tmp/kapt3/stubs/debug/com/example/contact/ClicListenner.java
+++
@@ -1,9 +0,0 @@
-package com.example.contact;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0000\b\u00e6\u0080\u0001\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/example/contact/ClicListenner;", "", "clic", "", "id", "", "app_debug"})
-public abstract interface ClicListenner {
-    
-    public abstract void clic(int id);
-}
